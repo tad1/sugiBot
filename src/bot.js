730f6158ad6e4d8635fdb00f8b6b9609c04d4508
row <rowwuk@users.noreply.github.com>
2020-12-10 13:44:30 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,15 +2,27 @@ require("dotenv").config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = "r-";
 
 client.on('ready', () => {
     console.log(`${client.user.tag} online.`);
 })
 
-client.on('message', (message) => {
-    console.log(`[${message.author.tag}]: ${message.content}`);
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+
     if (message.content === 'lol') {
-        message.reply('lol');
+        console.log(`[${message.author.tag}]: ${message.content}`);
+        message.channel.send('lol');
+    }
+
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+        console.log(CMD_NAME);
+        console.log(args);
     }
 })
 
