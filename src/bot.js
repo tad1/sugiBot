e630be30182871cedd5e300ccbab46c4d77e31f4
row <rowwuk@users.noreply.github.com>
2020-12-10 12:18:16 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,14 @@ const { Client } = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
-    console.log('The bot has logged in.');
+    console.log(`${client.user.tag} online.`);
+})
+
+client.on('message', (message) => {
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content === 'lol') {
+        message.reply('lol');
+    }
 })
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
