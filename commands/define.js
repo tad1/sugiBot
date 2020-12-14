3a1daf6726083899e21639faf6eb6919e3ed7220
= <tadziobrzeski@gmail.com>
2020-12-14 16:47:33 +0100

    Created Bot

---
+++ commands/define.js
@@ -0,0 +1,53 @@
+const fetch = require("node-fetch");
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "define",
+    description: "I will define any word you want!",
+    aliases: ['def'],
+    execute: async (message, args) => {
+        language = 'en';
+        word = 'define';
+        if(args.length){
+            
+            if(args.length >= 2){
+                language = args[0];
+                word = args[1];
+            } else {
+                word = args[0];
+            }
+
+            url = `https://api.dictionaryapi.dev/api/v2/entries/${language}/${word}`;
+            data = await fetch(url);
+            json = await data.json();
+
+            if(data.status != 200) return message.channel.send(json.title);
+
+            word = json[0];
+
+            const embedMessage = new Discord.MessageEmbed()
+            .setTitle(word.word);
+
+            let pronunciation = ""
+
+            for(element of word.phonetics){
+                pronunciation += (element.text + "  ");
+            }
+
+            embedMessage.setDescription(pronunciation);
+
+            for(meaning of word.meanings){
+                let definitions = '';
+                for(definition of meaning.definitions){
+                    definitions += '   ' + definition.definition + '\n';
+                }
+                embedMessage.addField(meaning.partOfSpeech, definitions);
+            }
+
+            return message.channel.send(embedMessage);
+            
+
+            
+        }
+    } 
+}
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "ping",
+    description: "Check your latency time",
+    aliases: ['🏓'],
+    execute: (message, args) => {
+        return message.channel.send(`Pong! 🏓 \nLatency is ${Date.now() - message.createdTimestamp}ms. \nAPI Latency is ${Math.round(message.client.ws.ping)}ms`);
+    } 
+}
