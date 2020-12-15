bfbefe559e52730302489e36a61c89ec9105f215
Sugimoto01 <lamkiewicz.kamil@gmail.com>
2020-12-15 09:24:50 +0100

    Update ban.js

--- SugiBot/src/commands/ban.js
+++ SugiBot/src/commands/ban.js
@@ -1,3 +1,5 @@
+const { GuildMember } = require("discord.js");
+
 module.exports = {
     name : 'ban',
     description : 'ban user',
@@ -6,12 +8,14 @@ module.exports = {
         if (!message.member.hasPermission('BAN_MEMBERS'))
         return message.reply("You do not have permissions to use that command");
       if (args.length === 0) return message.reply("Please provide an ID");
-      try {
-        const user =  await message.guild.members.ban(args[0]);
-        message.channel.send('User was banned successfully');
-      } catch (err) {
-        console.log(err);
-        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
+        const member =  message.guild.members.cache.get(args[0]);      
+      if (member) {
+        member
+          .ban( {days: 7, reason : 'Bo tak'})
+          .then((member) => message.channel.send(`${member} zostal zbanowany.`))
+          .catch((err) => message.channel.send('Nie moge zbanowac uzytkownika :('));
+      } else {
+        message.channel.send('Nie znaleziono uzytkownika');
       }
     },
 
