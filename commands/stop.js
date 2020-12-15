c55b7a8577624b15a236eba768f1a3aea4b67cce
= <tadziobrzeski@gmail.com>
2020-12-15 22:21:12 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'stop',
     description: 'On default: Say to bot that music he plays is ugly.\nThe bot will get mental brakdown and he will leave voice chat.\nPlease consider say a nice word to bot if you like him.',
 	aliases: ['s', 'leave', 'l'],
-	arguments: "`[message to bot that his music is fine, but you don't want to listen is now]`",
+	arguments: "`[message to bot that his music is fine, but you don't want to listen it now]`",
 	execute(message, args) {
 
 		if(!message.member.voice.channel) return message.channel.send("I don't see you on a voice channel with me");
