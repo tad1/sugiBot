fa9e2880b8f2ad923d9c29d66c4867c3a66f2c69
= <tadziobrzeski@gmail.com>
2020-12-15 17:37:56 +0100

    Updated stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -5,6 +5,7 @@ module.exports = {
 	arguments: "`[message to bot that his music is fine, but you don't want to listen is now]`",
 	execute(message, args) {
 
+		if(!message.member.voice.channel) return message.channel.send("I don't see you voice channel with me");
 		console.log(`[${message.author.tag}] requested leave`);
 
 		message.member.voice.channel.leave();
