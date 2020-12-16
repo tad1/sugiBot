b029a8c372fa7febe15274bf5d6904b40e5cc188
= <tadziobrzeski@gmail.com>
2020-12-16 14:43:18 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'stop',
-    description: 'On default: Say to bot that music he plays is ugly.\nThe bot will get mental brakdown and he will leave voice chat.\nPlease consider say a nice word to bot if you like him.',
+    description: 'On default: Say to bot that music he plays is ugly.\nThe bot will get mental brakdown and he will leave voice chat.\nPlease consider to say a nice word to bot if you like him.',
 	aliases: ['s', 'leave', 'l'],
 	arguments: "`[message to bot that his music is fine, but you don't want to listen it now]`",
 	execute(message, args) {
